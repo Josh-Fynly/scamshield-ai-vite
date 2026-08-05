ee4e6f48b7005745ee298eb780545a65abdd5243
Josh Fynly <196457925+Josh-Fynly@users.noreply.github.com>
2026-08-05 18:03:22 +0100

    Add index.ts to export UI components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,8 @@
+export { Button } from './Button';
+export { Card, CardHeader, CardContent, CardFooter } from './Card';
+export { Badge, RiskBadge } from './Badge';
+export { Input } from './Input';
+export { Textarea } from './Textarea';
+export { Modal } from './Modal';
+export { Select } from './Select';
+export { Spinner } from './Spinner';
